892cc28f7389e8bb318c71023112de3aec338218
wagnercos <wagcos@MacBook-Air-de-Wagner.local>
2023-05-27 09:57:03 -0300

    last commit

--- mobile/app/new.tsx
+++ mobile/app/new.tsx
@@ -1,4 +1,5 @@
-import { Link } from 'expo-router'
+import { useState } from 'react'
+import { Link, useRouter } from 'expo-router'
 import {
   Text,
   TouchableOpacity,
@@ -6,24 +7,87 @@ import {
   Switch,
   TextInput,
   ScrollView,
+  Image,
 } from 'react-native'
 import Icon from '@expo/vector-icons/Feather'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import * as ImagePicker from 'expo-image-picker'
+import * as SecureStore from 'expo-secure-store'
 
 import Logo from '../src/assets/logo.svg'
-import { useState } from 'react'
+import { api } from '../src/lib/api'
 
 export default function New() {
   const { bottom, top } = useSafeAreaInsets()
+  const router = useRouter()
 
+  const [preview, setPreview] = useState<string | null>(null)
+  const [content, setContent] = useState('')
   const [isPublic, setIsPublic] = useState(false)
+
+  async function openImagePicker() {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+      })
+
+      if (result.assets[0]) {
+        setPreview(result.assets[0].uri)
+      }
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  async function handleCreateMemory() {
+    const token = await SecureStore.getItemAsync('token')
+
+    let coverUrl = ''
+
+    if (preview) {
+      const uploadFormData = new FormData()
+
+      uploadFormData.append('file', {
+        uri: preview,
+        name: 'image.jpg',
+        type: 'image/jpeg',
+      } as any)
+
+      const uploadResponse = await api.post('/upload', uploadFormData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+      })
+
+      coverUrl = uploadResponse.data.fileUrl
+
+      console.log(coverUrl)
+    }
+
+    await api.post(
+      '/memories',
+      {
+        content,
+        isPublic,
+        coverUrl,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      },
+    )
+
+    router.push('/memories')
+  }
 
   return (
     <ScrollView
       className="flex-1 px-8"
       contentContainerStyle={{ paddingBottom: bottom, paddingTop: top }}
     >
-      <View className="mb-6 flex-row items-center justify-between">
+      <View className="mt-6 flex-row items-center justify-between">
         <Logo />
 
         <Link href="/memories" asChild>
@@ -50,24 +114,36 @@ export default function New() {
         </View>
 
         <TouchableOpacity
+          onPress={openImagePicker}
           activeOpacity={0.7}
           className="h-32 items-center justify-center rounded-lg border border-dashed border-gray-500 bg-black/20"
         >
-          <View className="flex-row items-center gap-2">
-            <Icon name="image" color="#fff" />
-            <Text className="font-body text-sm text-gray-200">
-              Adicionar foto ou vídeo de capa
-            </Text>
-          </View>
+          {preview ? (
+            <Image
+              source={{ uri: preview }}
+              className="h-full w-full rounded-lg object-cover"
+              alt=""
+            />
+          ) : (
+            <View className="flex-row items-center gap-2">
+              <Icon name="image" color="#fff" />
+              <Text className="font-body text-sm text-gray-200">
+                Adicionar foto ou vídeo de capa
+              </Text>
+            </View>
+          )}
         </TouchableOpacity>
         <TextInput
           multiline
+          value={content}
+          onChangeText={setContent}
           className="p-0 font-body text-lg text-gray-50"
           placeholderTextColor="#56565a"
           placeholder="Fique livre para adicionar fotos, vídeos e relatos sobre essa experiência que você quer lembrar para sempre."
         />
 
         <TouchableOpacity
+          onPress={handleCreateMemory}
           activeOpacity={0.7}
           className="items-center rounded-full bg-green-500 px-5 py-2"
         >
